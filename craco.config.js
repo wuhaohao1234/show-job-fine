4a27bf23cb7236ec89b5c094b56af168689156ae
wuhaohao1234 <1611499758@qq.com>
2022-06-29 17:50:03 +0800

    feat: docs

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,24 @@
 const CracoLessPlugin = require('craco-less');
-
+const path = require('path')
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, {
+      env, paths
+    }) => {
+      // paths.appPath='public'
+      paths.appBuild = 'dist'
+      webpackConfig.output = {
+        ...webpackConfig.output,
+          // ...{
+          //   filename: whenDev(() => 'static/js/bundle.js', 'static/js/[name].js'),
+          //   chunkFilename: 'static/js/[name].js'
+          // },
+          path: path.resolve(__dirname, 'docs'), // 修改输出文件目录
+          publicPath: 'show-job-fine'
+      }
+      return webpackConfig
+    }
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
